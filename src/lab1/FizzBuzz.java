925bb7b74d6544e0f327750d027a4f1de5aa9d44
Randal Baumer <baumer.randy@gmail.com>
2026-09-14 16:50:17 -0400

    Removed args from main()

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -5,7 +5,7 @@ package lab1;
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
 
         int j = 1;
 
@@ -39,8 +39,7 @@ class FizzBuzz {
 //            }
 //        }
     }
-
-    private static int doFizzBuzz(int j) {
+    public static int doFizzBuzz(int j) {
         boolean divisibleBy3 = j % 3 == 0;
         boolean divisibleBy5 = j % 5 == 0;
 
---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    static void main() {
+        FizzBuzz.doFizzBuzz(15);
+        FizzBuzz.main();
+    }
+}
+
